a5c360449b50a4bc0994f817e5545559cc62cd67
François Pottier <francois.pottier@inria.fr>
2020-02-28 09:12:58 +0100

    New exercise: anagrams.

---
+++ exercises/anagrams/right/with_a_hash_table.ml
@@ -0,0 +1,42 @@
+(* A histogram maps characters to integer frequencies. *)
+
+type histogram =
+  (char, int) Hashtbl.t
+
+(* Building a histogram out of a string. *)
+
+let histogram (s : string) : histogram =
+  let table = Hashtbl.create 256 in
+  String.iter (fun symbol ->
+    let freq = try Hashtbl.find table symbol with Not_found -> 0 in
+    Hashtbl.replace table symbol (freq + 1)
+  ) s;
+  table
+
+(* Converting a histogram to a list. *)
+
+let elements (h : histogram) : (char * int) list =
+  let elements = ref [] in
+  Hashtbl.iter (fun symbol freq ->
+    elements := (symbol, freq) :: !elements
+  ) h;
+  !elements
+
+(* Converting a histogram to a sorted list. *)
+
+let elements (h : histogram) : (char * int) list =
+  let cmp (c1, _) (c2, _) = compare c1 c2 in
+  List.sort cmp (elements h)
+
+(* Comparing two histograms. *)
+
+let eq_histogram (h1 : histogram) (h2 : histogram) : bool =
+  elements h1 = elements h2
+
+let anagrams (s1 : string) (s2 : string) : bool =
+  (* Two strings are anagrams of one another if and only if they have
+     the same histogram. As a fast path, we can first check if the two
+     strings have the same length; if they do not, they are definitely
+     not anagrams. *)
+  String.length s1 = String.length s2 &&
+  eq_histogram (histogram s1) (histogram s2)
